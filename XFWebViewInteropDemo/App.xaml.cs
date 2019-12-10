3794d4f543b4cf42fa90f8063e28d7f7476afc38
Udara Alwis <udara.robotics@gmail.com>
2019-12-10 20:17:09 +0800

    webview stuff

--- XFWebViewInteropDemo/App.xaml.cs
+++ XFWebViewInteropDemo/App.xaml.cs
@@ -10,7 +10,7 @@ namespace XFWebViewInteropDemo
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
