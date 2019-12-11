f2eb46de7cb66a75bb84750da8790bdf2ee47ad6
Udara Alwis <udara.robotics@gmail.com>
2019-12-11 13:37:08 +0800

    hybrid webview

--- XFWebViewInteropDemo/HybridWebViewDemoPage.xaml.cs
+++ XFWebViewInteropDemo/HybridWebViewDemoPage.xaml.cs
@@ -17,9 +17,59 @@ namespace XFWebViewInteropDemo
             InitializeComponent();
         }
 
-        private void sendToWebViewButton_Clicked(object sender, EventArgs e)
+        protected override void OnAppearing()
         {
-            
+            base.OnAppearing();
+
+            webViewElement.Source = new HtmlWebViewSource()
+            {
+                Html =
+                    $@"<html>" +
+                    "<head>" +
+                        "<meta charset=\"utf-8\">" +
+                        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">" +
+                        "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" " +
+                        "integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\" crossorigin=\"anonymous\">" +
+
+                        "<script type=\"text/javascript\">" +
+                            "function updatetextonwebview(text) {" +
+                            "    document.getElementById(\"textElement\").innerHTML = text;" +
+                            "}" +
+                            "function invokexamarinforms(){" +
+                            "    try{" +
+                            "        var inputvalue = document.getElementById(\"textElement\").value;" +
+                            "        invokeCSharpAction(inputvalue + '. This is from Javascript in the WebView!');" +
+                            "    }" +
+                            "    catch(err){" +
+                            "        alert(err);" +
+                            "    }" +
+                            "}" +
+                        "</script>" +
+                    "</head>" +
+
+                    "<body style=\"background-color: #d4ecff;padding: 20px; border: 1px solid #2196F3;border-radius: 5px;\">" +
+                        "<div>" +
+                            "<p class=\"h4\">This is a simple bootstrap based HTML Web page!</p><br />" +
+                            "<div id=\"textElement\" class=\"shadow p-3 mb-5 bg-white rounded\">" +
+                                "<span class=\"spinner-grow spinner-grow-sm\" role=\"status\" aria-hidden=\"true\" ></span>" +
+                                "  waiting for data from Xamarin.Forms..." +
+                            "</div>" +
+                        "</div>" +
+                        "<div class=\"form-group\">" +
+                            "<input type=\"text\" class=\"form-control\" id=\"textElement\" placeholder=\"type something here...\">" +
+                        "</div>" +
+                        "<div class=\"form-group\">" +
+                            "<button type=\"button\" class=\"btn btn-primary mb-2\" onclick=\"invokexamarinforms()\">Send to Xamarin.Forms</button>" +
+                        "</div>" +
+                    "</body>" +
+
+                    "</html>"
+            };
+        }
+
+        private async void sendToWebViewButton_Clicked(object sender, EventArgs e)
+        {
+            string result = await webViewElement.EvaluateJavaScriptAsync($"updatetextonwebview('{textEntryElement.Text}')");
         }
     }
 }
