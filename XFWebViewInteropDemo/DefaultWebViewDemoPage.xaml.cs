a0899503ab52e6aba6a52af295708b983c7f1ed8
Udara Alwis <udara.robotics@gmail.com>
2019-12-11 12:30:55 +0800

    separation of webviews

---
+++ XFWebViewInteropDemo/DefaultWebViewDemoPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFWebViewInteropDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DefaultWebViewDemoPage : ContentPage
+    {
+        public DefaultWebViewDemoPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            webViewElement.Source = new HtmlWebViewSource()
+            {
+                Html =
+                    $@"<html>" +
+                    "<head>" +
+                        "<meta charset=\"utf-8\">" +
+                        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">" +
+                        "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" " +
+                        "integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\" crossorigin=\"anonymous\">" +
+
+                        "<script type=\"text/javascript\">" +
+                            "function updatetextonwebview(text) {" +
+                            "    document.getElementById(\"textElement\").innerHTML = text;" +
+                            "}" +
+                        "</script>" +
+                    "</head>" +
+
+                    "<body style=\"background-color: #d4ecff;padding: 20px; border: 1px solid #2196F3;border-radius: 5px;\">" +
+                        "<div>" +
+                            "<p class=\"h4\">This is a simple bootstrap based HTML Webpage!</p><br />" +
+                            "<div id=\"textElement\" class=\"shadow p-3 mb-5 bg-white rounded\">" +
+                                "<span class=\"spinner-grow spinner-grow-sm\" role=\"status\" aria-hidden=\"true\" ></span>" +
+                                "  waiting for data from Xamarin.Forms..." +
+                            "</div>" +
+                        "</div>" +
+                    "</body>" +
+
+                    "</html>"
+            };
+        }
+
+        private async void sendToWebViewButton_Clicked(object sender, EventArgs e)
+        {
+            string result = await webViewElement.EvaluateJavaScriptAsync($"updatetextonwebview('{textEntryElement.Text}')");
+        }
+    }
+}
--- XFWebViewInteropDemo/MainPage.xaml.cs
+++ XFWebViewInteropDemo/MainPage.xaml.cs
@@ -18,44 +18,14 @@ namespace XFWebViewInteropDemo
             InitializeComponent();
         }
 
-        protected override void OnAppearing()
+        private async void GoToDefaultWebViewButton_OnClicked(object sender, EventArgs e)
         {
-            base.OnAppearing();
-
-            webViewElement.Source = new HtmlWebViewSource()
-            {
-                Html =
-                $@"<html>" +
-                     "<head>" +
-                         "<meta charset=\"utf-8\">" +
-                         "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">" +
-                         "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" " +
-                         "integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\" crossorigin=\"anonymous\">" +
-
-                         "<script type=\"text/javascript\">" +
-                             "function updatetextonwebview(text) {" +
-                             "    document.getElementById(\"textElement\").innerHTML = text;" +
-                             "}" +
-                         "</script>" +
-                     "</head>" +
-
-                     "<body style=\"background-color: #d4ecff;padding: 20px; border: 1px solid #2196F3;border-radius: 5px;\">" +
-                         "<div>" +
-                             "<p class=\"h4\">Hello there! this is a HTML Webpage!</p>" +
-                             "<div id=\"textElement\" class=\"shadow p-3 mb-5 bg-white rounded\">" +
-                                 "<span class=\"spinner-grow spinner-grow-sm\" role=\"status\" aria-hidden=\"true\" ></span>" +
-                                 "  waiting for data from Xamarin.Forms..." +
-                             "</div>" +
-                         "</div>" +
-                     "</body>" +
-
-                "</html>"
-            };
+            await Navigation.PushAsync(new DefaultWebViewDemoPage());
         }
 
-        private async void textEntryElement_OnTextChanged(object sender, TextChangedEventArgs e)
+        private async void GoToHybridWebViewButton_OnClicked(object sender, EventArgs e)
         {
-            string result = await webViewElement.EvaluateJavaScriptAsync($"updatetextonwebview('{textEntryElement.Text}')");
+            await Navigation.PushAsync(new HybridWebViewDemoPage());
         }
     }
 }
