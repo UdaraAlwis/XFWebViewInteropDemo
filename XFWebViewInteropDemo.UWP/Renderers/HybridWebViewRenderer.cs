480e34b07f09319c5026f6e0ec2913bcc7fe8f62
Udara Alwis <udara.robotics@gmail.com>
2020-01-14 23:49:19 +0800

    uwp hybrid screenshot and code

---
+++ XFWebViewInteropDemo.UWP/Renderers/HybridWebViewRenderer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using XFWebViewInteropDemo;
+using XFWebViewInteropDemo.UWP;
+using Xamarin.Forms.Platform.UWP;
+using Windows.UI.Xaml.Controls;
+using XFWebViewInteropDemo.Controls;
+using XFWebViewInteropDemo.UWP.Renderers;
+
+[assembly: ExportRenderer(typeof(HybridWebView), typeof(HybridWebViewRenderer))]
+namespace XFWebViewInteropDemo.UWP.Renderers
+{
+    public class HybridWebViewRenderer : WebViewRenderer
+    {
+        const string JavaScriptFunction = "function invokeCSharpAction(data){window.external.notify(data);}";
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.WebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+                Control.NavigationCompleted -= OnWebViewNavigationCompleted;
+                Control.ScriptNotify -= OnWebViewScriptNotify;
+            }
+            if (e.NewElement != null)
+            {
+                Control.NavigationCompleted += OnWebViewNavigationCompleted;
+                Control.ScriptNotify += OnWebViewScriptNotify;
+                //// No need this since we're loading dynamically generated HTML content
+                //Control.Source = new Uri($"ms-appx-web:///Content//{((HybridWebView)Element).Uri}");
+            }
+        }
+
+        async void OnWebViewNavigationCompleted(Windows.UI.Xaml.Controls.WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            if (args.IsSuccess)
+            {
+                // Inject JS script
+                await Control.InvokeScriptAsync("eval", new[] { JavaScriptFunction });
+            }
+        }
+
+        void OnWebViewScriptNotify(object sender, NotifyEventArgs e)
+        {
+            ((HybridWebView)Element).InvokeAction(e.Value);
+        }
+    }
+}
